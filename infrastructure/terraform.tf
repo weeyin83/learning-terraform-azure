e0861bbc15e3b854d9226be5ee6b58baae6c6a70
Sarah Lean <Sarah.Lean@microsoft.com>
2024-01-08 15:04:35 +0000

    update workspace

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -16,6 +16,14 @@ terraform {
       version = "~>3.6.0"
     }
   }
+    backend "remote" {
+    hostname = "backend.api.env0.com"
+    organization = "1ac843c4-9b54-4173-8633-0b179ec1c125"
+    
+    workspaces {
+      name = "learning-terraform-azure-infrastructure-75602104"
+    }
+  }
 }
 
 ## 
